d9dbdba143c9adda12dbd19551a35aa7cbebdcd6
Tae Hui Chang <jasmine878@yahoo.com>
2025-07-15 23:35:05 -0500

    Calculate current year

---
+++ BootcampPrep_Java/src/main/java/org/example/valuesAndTypes/CurrentYear.java
@@ -0,0 +1,16 @@
+package org.example.valuesAndTypes;
+
+public class CurrentYear {
+    /*
+Current Year
+Create an Arithmetic Expression that uses three mathematical operators (+,-,/,*) that returns the date of the current year.
+
+EX: 2000 + 18 - 2 * 1;
+
+*/
+// YOUR CODE BELOW
+    public static void main(String[] args) {
+        final int CURRENT_YEAR = 2000 + 10 * 3 - 5;
+        System.out.println(CURRENT_YEAR);
+    }
+}
