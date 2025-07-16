34625307977f8a86ce00936fe0fd5d480f659884
Tae Hui Chang <jasmine878@yahoo.com>
2025-07-15 23:37:20 -0500

    Make String uppercase

---
+++ BootcampPrep_Java/src/main/java/org/example/valuesAndTypes/ToUppercase.java
@@ -0,0 +1,17 @@
+package org.example.valuesAndTypes;
+
+public class ToUppercase {
+    public static void main(String[] args) {
+        /*
+To Uppercase
+Create a String by surrounding characters with an apostrophe or quotation marks. Call the toUpperCase() method on the string.
+
+EX: "apples".toUpperCase() OUTPUT: "APPLES"
+*/
+
+// YOUR CODE BELOW
+        String someString = "Hello World!";
+
+        System.out.println(someString.toUpperCase());
+    }
+}
