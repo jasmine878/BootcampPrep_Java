2adc116fd0ce01e0ccca00a1e3e4699b40b80796
Tae Hui Chang <jasmine878@yahoo.com>
2025-07-15 23:36:05 -0500

    Calculate a number

---
+++ BootcampPrep_Java/src/main/java/org/example/valuesAndTypes/MyNumber.java
@@ -0,0 +1,16 @@
+package org.example.valuesAndTypes;
+
+public class MyNumber {
+    /*
+My Age
+Create an Arithmetic Expression that uses four mathematical operators (+,-,/,*) that returns your current age.
+
+EX: (9+1)*6/2-1;
+*/
+// YOUR CODE BELOW
+    public static void main(String[] args) {
+        final int AGE = 20 / 2 * 3 - 9;
+
+        System.out.println(AGE);
+    }
+}
