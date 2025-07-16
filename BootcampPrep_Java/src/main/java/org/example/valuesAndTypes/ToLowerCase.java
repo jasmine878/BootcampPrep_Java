044520603ac5f85ab23a344bf62315bb632614db
Tae Hui Chang <jasmine878@yahoo.com>
2025-07-15 23:38:10 -0500

    Make String lowercase

---
+++ BootcampPrep_Java/src/main/java/org/example/valuesAndTypes/ToLowerCase.java
@@ -0,0 +1,19 @@
+package org.example.valuesAndTypes;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        /*
+To Lowercase
+Create a String by surrounding characters with an apostrophe or quotation marks. Call the toLowerCase() method on the string.
+
+EX: "FULLSTACK ACADEMY".toLowerCase() OUTPUT: "fullstack academy";
+*/
+
+// YOUR CODE BELOW
+
+        String st = "WHAT A CALM DAY";
+
+        System.out.println(st.toLowerCase());
+
+    }
+}
