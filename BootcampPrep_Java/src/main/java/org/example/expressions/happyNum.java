aa48494ef03963f20c25f937362550d4d9dcdfd8
Tae Hui Chang <jasmine878@yahoo.com>
2025-07-28 12:25:45 -0500

    reassign values

---
+++ BootcampPrep_Java/src/main/java/org/example/expressions/happyNum.java
@@ -0,0 +1,12 @@
+package org.example.expressions;
+
+public class happyNum {
+    public static void main(String[] args) {
+        int happyNum = 45;
+        happyNum += 10;
+        happyNum -= 5;
+        happyNum /= 10;
+
+        System.out.println(happyNum == 5);
+    }
+}
