2cfa1a6ca7eaac2e9cfe50faecb24afdcd5ab4c1
Tae Hui Chang <jasmine878@yahoo.com>
2025-07-28 12:05:45 -0500

    sum up variables

---
+++ BootcampPrep_Java/src/main/java/org/example/expressions/firstSum.java
@@ -0,0 +1,12 @@
+package org.example.expressions;
+
+public class firstSum {
+    public static void main(String[] args) {
+        int num1 = 854;
+        int num2 = 385;
+        int num3 = 779;
+
+        int myFirstSum = num1 + num2 + num3;
+        System.out.println(myFirstSum);
+    }
+}
