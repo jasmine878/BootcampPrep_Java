d65ead11e5389c21a3b601ddb9d72c1fc6d9987e
Tae Hui Chang <jasmine878@yahoo.com>
2025-07-28 12:11:58 -0500

    evaluate the expression

---
+++ BootcampPrep_Java/src/main/java/org/example/expressions/expression.java
@@ -0,0 +1,10 @@
+package org.example.expressions;
+
+public class expression {
+    public static void main(String[] args) {
+        int expression = 20 * (50 / (5 * 2) + 15);
+        int myGuess = 400;
+
+        System.out.println(expression == myGuess);
+    }
+}
