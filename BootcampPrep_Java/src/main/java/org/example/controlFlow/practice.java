e643b6d099f8f50f76b7ca03f074b1e3408fc52c
Tae Hui Chang <jasmine878@yahoo.com>
2025-08-23 14:53:00 -0500

    Use logical operators

--- BootcampPrep_Java/src/main/java/org/example/controlFlow/practice.java
+++ BootcampPrep_Java/src/main/java/org/example/controlFlow/practice.java
@@ -9,10 +9,22 @@ public class practice {
         return false;
     }
 
+    public String perfectMatch(String word) {
+        if(word.charAt(0) == 'p' && word.length() == 7) {
+            return word.toUpperCase();
+        }
+
+        return word;
+    }
+
     public static void main(String[] args) {
         practice test = new practice();
-        System.out.println(test.founderFinder("David"));
-        System.out.println(test.founderFinder("Nimit"));
-        System.out.println(test.founderFinder("Someone else"));
+//        System.out.println(test.founderFinder("David"));
+//        System.out.println(test.founderFinder("Nimit"));
+//        System.out.println(test.founderFinder("Someone else"));
+
+        System.out.println(test.perfectMatch("carrots"));
+        System.out.println(test.perfectMatch("pacific"));
+        System.out.println(test.perfectMatch("perfecto"));
     }
 }
