49b86bd49170df42540af91211eb5f3609bf8d68
Tae Hui Chang <jasmine878@yahoo.com>
2025-08-23 14:58:36 -0500

    Use logical operators

--- BootcampPrep_Java/src/main/java/org/example/controlFlow/practice.java
+++ BootcampPrep_Java/src/main/java/org/example/controlFlow/practice.java
@@ -17,14 +17,25 @@ public class practice {
         return word;
     }
 
+    public String truthTeller(boolean arg1, boolean arg2) {
+        if(arg1 && arg2) return "both";
+        if (arg1 || arg2) return "one";
+
+        return "none";
+    }
+
     public static void main(String[] args) {
         practice test = new practice();
 //        System.out.println(test.founderFinder("David"));
 //        System.out.println(test.founderFinder("Nimit"));
 //        System.out.println(test.founderFinder("Someone else"));
 
-        System.out.println(test.perfectMatch("carrots"));
-        System.out.println(test.perfectMatch("pacific"));
-        System.out.println(test.perfectMatch("perfecto"));
+//        System.out.println(test.perfectMatch("carrots"));
+//        System.out.println(test.perfectMatch("pacific"));
+//        System.out.println(test.perfectMatch("perfecto"));
+
+        System.out.println(test.truthTeller(true, true));
+        System.out.println(test.truthTeller(true, false));
+        System.out.println(test.truthTeller(false, false));
     }
 }
