ea539e81860346d51f6997d549acf62cd88ab4b4
Tae Hui Chang <jasmine878@yahoo.com>
2025-08-25 21:45:23 -0500

    add a default greeting

--- BootcampPrep_Java/src/main/java/org/example/practice/Functions.java
+++ BootcampPrep_Java/src/main/java/org/example/practice/Functions.java
@@ -5,12 +5,24 @@ public class Functions {
         return num1 + num2;
     }
 
+    public String defaultGreet(String firstName, String lastName) {
+        if (lastName == null) lastName = "Doe";
+
+        return "Hi " + firstName + " " + lastName + "!";
+    }
+
     public static void main(String[] args) {
         Functions test1 = new Functions();
 
-        System.out.println(test1.simpleSum(1, 2));
+//        System.out.println(test1.simpleSum(1, 2));
         //expect 3
-        System.out.println(test1.simpleSum(-10, -20));
+//        System.out.println(test1.simpleSum(-10, -20));
         //expect -30
+
+        System.out.println(test1.defaultGreet("Natalie", "Cole"));
+        //expect "Hi Natalie Cole!"
+        System.out.println(test1.defaultGreet("Jane", null));
+        //expect "Hi Jane Doe!"
+
     }
 }
