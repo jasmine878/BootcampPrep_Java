4883f7d196a59a8db05a7a1c131054f536331924
Tae Hui Chang <jasmine878@yahoo.com>
2025-09-13 00:10:30 -0500

    Use lastIndexOf method

--- BootcampPrep_Java/src/main/java/org/example/Arrays/LastIndex.java
+++ BootcampPrep_Java/src/main/java/org/example/Arrays/LastIndex.java
@@ -1,5 +1,7 @@
 package org.example.Arrays;
 
+import java.util.Arrays;
+
 public class LastIndex {
     public static int myLastIndexOf(int[]numbers, int searchItem, int startIdx) {
         int foundIdx = -1;
@@ -25,10 +27,23 @@ public class LastIndex {
         return foundIdx;
     }
 
+    public static int lastIndexOf(Integer[] numbers, int searchItem, int startIdx) {
+        return Arrays.asList(numbers).lastIndexOf(searchItem);
+    }
+
+    public static int lastIndexOf(String[] words, String searchItem, int startIdx) {
+        return Arrays.asList(words).lastIndexOf(searchItem);
+    }
+
     public static void main(String[] args) {
         System.out.println(myLastIndexOf(new int[] {0, 10, 20, 10, 0}, 10, 0)); //expect 3
         System.out.println(myLastIndexOf(new String[] {"peanut", "cashew", "walnut"}, "lemon", 0));  //expect -1
         System.out.println(myLastIndexOf(new String[] {"gee", "gee", "gee", "gee", "baby", "baby"}, "gee", 0));  //expect 3
+        System.out.println();
+
+        System.out.println(lastIndexOf(new Integer[] {0, 10, 20, 10, 0}, 10, 0));  //expect 3
+        System.out.println(lastIndexOf(new String[] {"Tiffany", "Sunny", "Yoona"}, "Jessica", 0));  //expect -1
+        System.out.println(lastIndexOf(new String[] {"the", "girls", "bring", "the", "boys", "out"}, "the", 0));  //expect 3
     }
 }
 
