ff8c13f290c2ab487d99375dbbd7ad0fffe5a2f2
Tae Hui Chang <jasmine878@yahoo.com>
2025-09-12 23:50:56 -0500

    Create lastIndexOf method

---
+++ BootcampPrep_Java/src/main/java/org/example/Arrays/LastIndex.java
@@ -0,0 +1,41 @@
+package org.example.Arrays;
+
+public class LastIndex {
+    public static int myLastIndexOf(int[]numbers, int searchItem, int startIdx) {
+        int foundIdx = -1;
+
+        for (int i = startIdx; i < numbers.length; i++) {
+            int number = numbers[i];
+
+            if (number == searchItem) foundIdx = i;
+        }
+
+        return foundIdx;
+    }
+
+    public static int myLastIndexOf(String[] words, String searchItem, int startIdx) {
+        int foundIdx = -1;
+
+        for (int i = startIdx; i < words.length; i++) {
+            String word = words[i];
+
+            if (word.equals(searchItem)) foundIdx = i;
+        }
+
+        return foundIdx;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myLastIndexOf(new int[] {0, 10, 20, 10, 0}, 10, 0)); //expect 3
+        System.out.println(myLastIndexOf(new String[] {"peanut", "cashew", "walnut"}, "lemon", 0));  //expect -1
+        System.out.println(myLastIndexOf(new String[] {"gee", "gee", "gee", "gee", "baby", "baby"}, "gee", 0));  //expect 3
+    }
+}
+
+//start searching from the start index and move towards the front of the array
+//return the last index of an array where the search item occurs
+//return -1 if the search item does not appear in the array
+
+//iterate through the array from startIdx
+//update the foundIdx as we find the searchItem
+//return the most up-to-date foundIdx
