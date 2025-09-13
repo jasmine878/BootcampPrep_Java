07c7a808149ab023c2fcd966d174277c2c9cb8aa
Tae Hui Chang <jasmine878@yahoo.com>
2025-09-12 19:36:36 -0500

    Use Streams anyMatch method

--- BootcampPrep_Java/src/main/java/org/example/Arrays/Contains.java
+++ BootcampPrep_Java/src/main/java/org/example/Arrays/Contains.java
@@ -1,5 +1,7 @@
 package org.example.Arrays;
 
+import java.util.Arrays;
+
 public class Contains {
     public static boolean myIncludes(int[] numbers, int searchNum) {
         for (int num: numbers) {
@@ -17,13 +19,31 @@ public class Contains {
         return false;
     }
 
+    public static boolean myAnyMatch(int[] numbers, int searchNum) {
+        if (Arrays.stream(numbers).anyMatch(num -> num == searchNum)) return true;
+
+        return false;
+    }
+
+    public static boolean myAnyMatch2(String[] words, String searchWord) {
+        if (Arrays.stream(words).anyMatch(word -> word.equals(searchWord))) return true;
+
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println(myIncludes(new int[] {10, 20, 30}, 20));  //expect true
         System.out.println(myIncludes2(new String[] {"apples", "bananas", "citrus"}, "pears"));  //expect false
         System.out.println(myIncludes2(new String[] {"ruby", "go", "javascript"}, "javascript"));   //expect true
+        System.out.println();
+
+        System.out.println(myAnyMatch(new int[] {1, 2, 3}, 3)); //expect true
+        System.out.println(myAnyMatch2(new String[] {"fun", "happy things", "kittens"}, "kittens")); //expect true
+        System.out.println(myAnyMatch(new int[] {5, 10, 15}, 20));  //expect false
+
     }
 }
 
 
-//iterate through the array, if we find the search item, return true
-//else return false
+// iterate through the array, if we find the search item, return true
+// else return false
