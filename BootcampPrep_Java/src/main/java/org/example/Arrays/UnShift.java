1402491b075e46645c5dac0dc132450432d6c14e
Tae Hui Chang <jasmine878@yahoo.com>
2025-09-14 00:23:05 -0500

    Use the addAll LinkedList method

--- BootcampPrep_Java/src/main/java/org/example/Arrays/UnShift.java
+++ BootcampPrep_Java/src/main/java/org/example/Arrays/UnShift.java
@@ -1,6 +1,8 @@
 package org.example.Arrays;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class UnShift {
     public static int[] myUnshift(int[] numbers, int num) {
@@ -25,9 +27,29 @@ public class UnShift {
         return result;
     }
 
+    public static Integer[] addAll(Integer[] numbers, int number) {
+        LinkedList<Integer> result = new LinkedList<>();
+        result.addAll(List.of(numbers));
+        result.addFirst(number);
+
+        return result.toArray(new Integer[0]);
+    }
+
+    public static String[] addAll(String[] words, String word) {
+        LinkedList<String> result = new LinkedList<>();
+        result.addAll(List.of(words));
+        result.addFirst(word);
+
+        return result.toArray(new String[0]);
+    }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(myUnshift(new int[] {1, 2, 3}, 0)));  //expect [0, 1, 2, 3]
         System.out.println(Arrays.toString(myUnshift(new String[] {"bears", "cows", "ducks"}, "antelopes")));   //expect ["antelopes", "bears", "cows", "ducks"]
+        System.out.println();
+
+        System.out.println(Arrays.toString(addAll(new Integer[] {5, 10, 15}, 2)));  //expect [2, 5, 10, 15]
+        System.out.println(Arrays.toString(addAll(new String[] {"Berry", "Corn", "Dates"}, "Apples")));  //expect ["Apples", "Berry", "Corn", "Dates"]
     }
 }
 
