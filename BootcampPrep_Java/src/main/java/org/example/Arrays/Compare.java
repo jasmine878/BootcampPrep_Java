c3f613547be9186bf6354828d3bda2cde5b38bbf
Tae Hui Chang <jasmine878@yahoo.com>
2025-09-22 00:53:47 -0500

    Compare Map Objects using Generics

--- BootcampPrep_Java/src/main/java/org/example/Arrays/Compare.java
+++ BootcampPrep_Java/src/main/java/org/example/Arrays/Compare.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Compare {
-    public static boolean compareObjects(Map<String, String> obj1, Map<String, String> obj2) {
+    public static <K, V> boolean compareObjects(Map<K, V> obj1, Map<K, V> obj2) {
         return obj1.equals(obj2);
     }
 
@@ -18,5 +18,15 @@ public class Compare {
 
         obj2.put("name", "zeke");
         System.out.println(compareObjects(obj1, obj2));         //expect false
+
+        //THESE HASHMAPS ARE IMMUTABLE DUE TO MAP.OF() METHOD
+        Map<String, Integer> obj3 = Map.of("a", 1);
+        Map<String, Integer> obj4 = Map.of("a", 1);
+        Map<String, Integer> obj5 = Map.of("b", 1);
+        Map<String, Integer> obj6 = Map.of("a", 2);
+
+        System.out.println(compareObjects(obj3, obj4));         //expect true
+        System.out.println(compareObjects(obj3, obj5));         //expect false
+        System.out.println(compareObjects(obj3, obj5));         //expect false
     }
 }
