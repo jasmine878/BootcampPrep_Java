9310b1a3c7cbff04cb83246ebf345ee99a8c0e6d
Tae Hui Chang <jasmine878@yahoo.com>
2025-09-21 21:05:33 -0500

    Use Hashmap for transactions

--- BootcampPrep_Java/src/main/java/org/example/Arrays/KatyPerry.java
+++ BootcampPrep_Java/src/main/java/org/example/Arrays/KatyPerry.java
@@ -1,5 +1,8 @@
 package org.example.Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KatyPerry {
     public int lastFridayNight(Transaction[] transactions) {
         int total = 0;
@@ -11,8 +14,19 @@ public class KatyPerry {
         return total;
     }
 
+    public int lastFridayNight(Map<String, Integer> transactionsMap) {
+        int total = 0;
+
+        for (int cost: transactionsMap.values()) {
+            total += cost;
+        }
+
+        return total;
+    }
+
     public static void main(String[] args) {
         KatyPerry test = new KatyPerry();
+
         Transaction[] transactions = new Transaction[] {
                 new Transaction("Tons of glitter", 70),
                 new Transaction("Porcelain Pink Flamingos", 92),
@@ -21,5 +35,12 @@ public class KatyPerry {
         };
 
         System.out.println(test.lastFridayNight(transactions));         // expect 10512
+
+        Map<String, Integer> transactions2 = new HashMap<>();
+        transactions2.put("apples", 5);
+        transactions2.put("kleenex", 2);
+        transactions2.put("knitting supplies", 10);
+
+        System.out.println(test.lastFridayNight(transactions2));        //expect 17
     }
 }
